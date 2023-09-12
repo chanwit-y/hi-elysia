c94e0b0eaa97dd706fbd49de5d78918e5a987f97
cy <chanwit.yimneam@hotmail.com>
2023-09-12 09:18:05 +0700

    graphql

---
+++ src/graphql.ts
@@ -0,0 +1,31 @@
+import { Elysia } from 'elysia'
+import { apollo, gql } from '@elysiajs/apollo'
+
+const app = new Elysia()
+    .use(
+        apollo({
+            typeDefs: gql`
+                type Book {
+                    title: String
+                    author: String
+                }
+
+                type Query {
+                    books: [Book]
+                }
+            `,
+            resolvers: {
+                Query: {
+                    books: () => {
+                        return [
+                            {
+                                title: 'Elysia',
+                                author: 'saltyAom'
+                            }
+                        ]
+                    }
+                }
+            }
+        })
+    )
+    .listen(8080)
