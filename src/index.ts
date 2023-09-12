301e8579ff1f7146c44cfd450f4939f12b1b2a17
cy <chanwit.yimneam@hotmail.com>
2023-09-12 09:00:58 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+new Elysia()
+  .use(
+    swagger({
+      path: "/v2/swagger",
+    })
+  )
+  .get("/", () => "hi")
+  .post("/hello/:id", ({ params: { id } }) => {
+    return id;
+  })
+  .post(
+    "/a",
+    ({ body }) => {
+      console.log(body);
+      return "hi";
+    },
+    {
+      body: t.Object({
+        a: t.String(),
+        b: t.Number(),
+        c: t.Boolean(),
+        d: t.Array(t.String()),
+        e: t.Object({
+          f: t.String(),
+          g: t.Number(),
+          h: t.Boolean(),
+          i: t.Array(t.String()),
+        }),
+      }),
+    }
+  )
+  .listen(8080);
